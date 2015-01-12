918b0aaa00c35326f2e870107ebdb03b19558f3a
ROHIT <iosdeveloperindia@gmail.com>
2015-01-12 19:16:26 +0530

    Initial Commit

---
+++ Saje/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Saje
+//
+//  Created by Nagendra Tripathi on 12/01/15.
+//  Copyright (c) 2015 Rohit Tripathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Saje/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Saje
+//
+//  Created by Nagendra Tripathi on 12/01/15.
+//  Copyright (c) 2015 Rohit Tripathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
